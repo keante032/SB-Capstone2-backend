5b65b0c848ea61104106d59b02a4fefcdff37292
Kevin Templeton <templeton.kevin@gmail.com>
2023-03-22 08:54:13 -0500

    added error handlers

--- api/app.js
+++ api/app.js
@@ -13,11 +13,30 @@ app.use("/auth", authRoutes);
 app.use("/recipes", recipesRoutes);
 app.use("/users", usersRoutes);
 
+/** Avoid CORS issues */
 app.use(function (req, res, next) {
     // TODO: update '*' to match the domain of deployed frontend
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
+});
+
+/** Handle 404 errors for everything that doesn't match a route */
+app.use(function (req, res, next) {
+    return next(new Error("Not Found"));
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    // log the error stack in development and production modes, but not in test mode
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    // the default status is 500 Internal Server Error
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status }
+    });
 });
 
 module.exports = app;
