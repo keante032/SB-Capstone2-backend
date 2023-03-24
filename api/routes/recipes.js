2f9cfddd0e184bd395bff9dc0f35fe604372e7c4
Kevin Templeton <templeton.kevin@gmail.com>
2023-03-24 12:21:14 -0500

    recipe routes finished

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -1,21 +1,77 @@
 const express = require("express");
 
+const Recipe = require("../models/recipe");
+const { ensureLoggedIn } = require("../middleware/auth");
+const { validateRecipeSchema } = require("../middleware/schemaValidator");
+
 const router = new express.Router();
 
-router.get("/", function (req, res, next) {
-    // TODO: retrieve recipe data from database and put it in res.send()
+/** Get details of recipe
+ * GET /recipes/:id
+ * expects req.headers.authorization to be a valid token obtained from /auth/register or /auth/token,
+ * but only if the recipe is private; if it's public, no user info is needed
+ * returns { recipe: { id, ownerId, public, name, description, ingredients, directions } }
+ */
+router.get("/:id", async function (req, res, next) {
+    try {
+        const { username } = res.locals;
+        const { id } = req.params;
+        const recipe = await Recipe.get(username, id);
+        return res.json({ recipe });
+    } catch (err) {
+        return next(err);
+    }
 });
 
-router.post("/", function (req, res, next) {
-    // TODO: add new recipe to database
+/** Add new recipe
+ * POST /recipes/
+ * expects req.headers.authorization to be a valid token obtained from /auth/register or /auth/token
+ * expects { public, name, description, ingredients, directions }
+ * returns { recipe: { id, ownerId, public, name, description, ingredients, directions } }
+ */
+router.post("/", ensureLoggedIn, validateRecipeSchema, async function (req, res, next) {
+    try {
+        const { username } = res.locals;
+        const recipe = await Recipe.add(username, req.body);
+        return res.json({ recipe });
+    } catch (err) {
+        return next(err);
+    }
 });
 
-router.patch("/", function (req, res, next) {
-    // TODO: update existing recipe in database
+/** Update existing recipe
+ * PUT /recipes/:id
+ * expects req.headers.authorization to be a valid token obtained from /auth/register or /auth/token
+ * expects { public, name, description, ingredients, directions }
+ * returns { recipe: { id, ownerId, public, name, description, ingredients, directions } }
+ */
+router.put("/:id", ensureLoggedIn, validateRecipeSchema, async function (req, res, next) {
+    try {
+        const { username } = res.locals;
+        const { id } = req.params;
+        // inside of Recipe.edit, we will check if the user is the owner of the recipe
+        const recipe = await Recipe.edit(username, id, req.body);
+        return res.json({ recipe });
+    } catch (err) {
+        return next(err);
+    }
 });
 
-router.delete("/", function (req, res, next) {
-    // TODO: delete recipe from database
+/** Delete existing recipe
+ * DELETE /recipes/:id
+ * expects req.headers.authorization to be a valid token obtained from /auth/register or /auth/token
+ * returns { deleted: { recipeId, recipeName, message } }
+ */
+router.delete("/:id", ensureLoggedIn, async function (req, res, next) {
+    try {
+        const { username } = res.locals;
+        const { id } = req.params;
+        // inside of Recipe.delete, we will check if the user is the owner of the recipe
+        const deleted = await Recipe.delete(username, id);
+        return res.json({ deleted });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 
