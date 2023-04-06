ccf801dba3ffe9e341c5c552bc9a10d330b0f52c
Kevin Templeton <templeton.kevin@gmail.com>
2023-04-05 22:15:49 -0500

    adding components to routes

--- client/src/navigation/Routes.js
+++ client/src/navigation/Routes.js
@@ -1,16 +1,34 @@
 import { BrowserRouter as Switch, Route, Redirect } from 'react-router-dom';
 import PrivateRoute from "./PrivateRoute";
+import Home from "../routes/Home";
+import PublicRecipes from "../routes/PublicRecipes";
+import Register from "../routes/Register";
+import Login from "../routes/Login";
+import Dashboard from "../routes/Dashboard";
+import RecipeSearch from "../routes/RecipeSearch";
 
-function Routes() {
+function Routes({ login, register }) {
     return (
         <div>
             <Switch>
-                <Route exact path="/"></Route>
-                <Route exact path="/public-recipes"></Route>
-                <Route exact path="/register"></Route>
-                <Route exact path="/login"></Route>
-                <PrivateRoute exact path="/dashboard"></PrivateRoute>
-                <PrivateRoute exact path="/recipe-search"></PrivateRoute>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/public-recipes">
+                    <PublicRecipes />
+                </Route>
+                <Route exact path="/register">
+                    <Register register={register} />
+                </Route>
+                <Route exact path="/login">
+                    <Login login={login} />
+                </Route>
+                <PrivateRoute exact path="/dashboard">
+                    <Dashboard />
+                </PrivateRoute>
+                <PrivateRoute exact path="/recipe-search">
+                    <RecipeSearch />
+                </PrivateRoute>
                 <Redirect to="/" />
             </Switch>
         </div>
