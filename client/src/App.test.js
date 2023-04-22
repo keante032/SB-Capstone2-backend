7a77c0e9d498057f33e10fe0d09a842a3e059976
Kevin Templeton <templeton.kevin@gmail.com>
2023-04-22 13:26:12 -0500

    testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders without crashing', () => {
+    render(<App />);
+})
+
+test('renders navbar', () => {
+    render(<App />);
+    const navbar = screen.getByRole('navigation');
+    expect(navbar).toBeInTheDocument();
+})
+
